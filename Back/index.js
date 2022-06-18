d3a4e8e63e6bf5d9d46e12e0f10e02241805e1ec
Jhuliana <jholeanasantos@gmail.com>
2022-06-17 21:22:18 -0300

    feat: sign-up

--- Back/index.js
+++ Back/index.js
@@ -1,9 +1,34 @@
-import express  from "express";
+import express from "express";
+import cors from 'cors';
 
 const app = express();
+app.use(express.json())
+app.use(cors());
 
+const userData = []
+const tweets = []
+    
 
+app.post("/sign-up", (req, res) => {
+        const user = { ...req.body }
+        userData.push(user)
+        res.status(201).send("OK")
+    })
 
+app.post("/tweets", (req, res) => { 
+    const tweet = req.body
+    for(let i = 0; userData.length > i; i++){
+        if (tweet.username  === userData[i].username) {
+            tweet["avatar"] = userData[i].avatar
+        }
+    }
+    tweets.unshift(tweet)
+    res.status(201).send("OK")
+})
+
+app.get("/tweets", (req, res) => {   
+    res.send(tweets.filter((_,index)=> index <= 9))
+})
 
 
 app.listen(5000)
